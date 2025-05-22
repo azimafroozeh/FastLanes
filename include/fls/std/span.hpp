f69e011b9a2979f473b634bbeee122aa5783419b
azimafroozeh <afroozeh3@gmail.com>
2025-05-22 20:13:02 +0200

    try this

--- include/fls/std/span.hpp
+++ include/fls/std/span.hpp
@@ -1,21 +1,10 @@
 #ifndef FLS_STD_SPAN_HPP
 #define FLS_STD_SPAN_HPP
 
-// Prefer the real C++20 header if the toolchain has it
-#if __has_include(<span>)
 #include <span>
-#endif
 
-// If <span> is missing—or present but still no std::span—fall back
-#ifndef __cpp_lib_span
-#include <flatbuffers/stl_emulation.h> // flatbuffers::span poly-fill
-namespace fastlanes {
-using flatbuffers::span;
-}
-#else
 namespace fastlanes {
 using std::span;
-}
-#endif
-#endif
+} // namespace fastlanes
 
+#endif // FLS_STD_SPAN_HPP
