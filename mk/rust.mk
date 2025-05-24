5dba0cb2184ae6ba89982f346a31151d04207e70
azimafroozeh <afroozeh3@gmail.com>
2025-05-24 20:05:44 +0200

    try this

--- mk/rust.mk
+++ mk/rust.mk
@@ -73,7 +73,7 @@ clean-rust: clean-vendor
 	$(CARGO) clean --manifest-path $(CRATE_ROOT)/Cargo.toml
 	@echo "Rust clean complete."
 
-run-rust-example: build-rust
+run-rust-example: version-bump-rust
 	@echo "Running Rust example ‘rust_example’…"
 	cd $(CRATE_ROOT) && \
 	CC="$(CC)" CXX="$(CXX)" \
