159dd4c5d5813bc44ef9ce19a012b7828ac51a3e
azimafroozeh <afroozeh3@gmail.com>
2025-05-25 02:07:45 +0200

    Refresh vendored FastLanes sources from workspace

--- rust/vendor/fastlanes/include/fls/info.hpp
+++ rust/vendor/fastlanes/include/fls/info.hpp
@@ -88,7 +88,7 @@ public:
 
 private:
 	static constexpr auto lib_name        = string_view("FastLanes");
-	static constexpr auto current_version = string_view("0.1.3-alpha.4"); // smart entry point
+	static constexpr auto current_version = string_view("0.1.3-alpha.5"); // smart entry point
 };
 
 } // namespace fastlanes
