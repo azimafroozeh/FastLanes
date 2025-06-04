11f71b27bc65449ea4eb4d5837c675045a1fdfa6
Azim Afroozeh <afroozeh3@gmail.com>
2025-06-05 01:45:37 +0200

    Fix opend_app comment

--- src/include/fls/std/filesystem.hpp
+++ src/include/fls/std/filesystem.hpp
@@ -19,8 +19,8 @@ public:
 	static std::ofstream open_w(const path& file);
 	///! open write in binary
 	static std::ofstream open_w_binary(const path& file);
-	///! open write in binary
-	static std::ofstream opend_app(const path& file);
+        ///! open file for appending
+        static std::ofstream opend_app(const path& file);
 	///! close
 	template <typename STREAM>
 	static void close(STREAM& stream);
