c4e998508c60e4d3da3701170be57b28c7933606
azimafroozeh <afroozeh3@gmail.com>
2025-07-21 23:35:55 +0200

    init galp

---
+++ test/src/expression_tests/galp_test.cpp
@@ -0,0 +1,18 @@
+#include "fls_tester.hpp"
+
+namespace fastlanes {
+
+TEST_F(FastLanesReaderTester, TEST_GALP_FLOAT_ONE_VECTOR) {
+	TestCorrectness(galp::galp_one_vector, {OperatorToken::EXP_GALP_FLT});
+}
+TEST_F(FastLanesReaderTester, TEST_GALP_FLOAT_SINGLE_COLUMN) {
+	TestCorrectness(galp::galp_single_column, {OperatorToken::EXP_GALP_FLT});
+}
+TEST_F(FastLanesReaderTester, TEST_GALP_FLOAT_CONSTANT_ONE_VECTOR) {
+	TestCorrectness(galp::constant_one_vector, {OperatorToken::EXP_GALP_FLT});
+}
+TEST_F(FastLanesReaderTester, TEST_GALP_FLOAT_CONSTANT_SINGLE_COLUMN) {
+	TestCorrectness(galp::constant_single_column, {OperatorToken::EXP_GALP_FLT});
+}
+
+} // namespace fastlanes
